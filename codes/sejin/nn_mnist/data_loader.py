9e12f0246b80ce43549db4fa9a5577b11f37eab3
Sejin Jeon <jinaidy93@gmail.com>
2018-09-06 10:32:20 +0900

    Create data_loader.py

---
+++ codes/sejin/nn_mnist/data_loader.py
@@ -0,0 +1,52 @@
+import argparse
+import torch
+from torchvision import datasets, transforms
+
+args = None
+kargs = None
+
+def init_arguments():
+    global args
+    global kwargs
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    args = parser.parse_args()
+    kwargs = {'num_workers': 1, 'pin_memory': True}
+
+def load_mnist_data():
+    global args
+    global kargs
+    init_arguments()
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(root='../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs
+    )
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+    return train_loader, test_loader
