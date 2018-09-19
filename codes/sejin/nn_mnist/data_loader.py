3a713e4055b009155addae8b529334723be39147
Sejin Jeon <jinaidy93@gmail.com>
2018-09-19 10:34:48 +0900

    WIP: Create a vanilla neural network model

--- codes/sejin/nn_mnist/data_loader.py
+++ codes/sejin/nn_mnist/data_loader.py
@@ -2,36 +2,30 @@ import argparse
 import torch
 from torchvision import datasets, transforms
 
-args = None
-kargs = None
-
-def init_arguments():
-    global args
-    global kwargs
+def init_arguments(init_data):
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=init_data.batch_size, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+    parser.add_argument('--test-batch-size', type=int, default=init_data.test_batch_size, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+    parser.add_argument('--epochs', type=int, default=init_data.epochs, metavar='N',
                         help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+    parser.add_argument('--lr', type=float, default=init_data.lr, metavar='LR',
                         help='learning rate (default: 0.01)')
-    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+    parser.add_argument('--momentum', type=float, default=init_data.momentum, metavar='M',
                         help='SGD momentum (default: 0.5)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
+    parser.add_argument('--no-cuda', action='store_true', default=init_data.no_cuda,
                         help='disables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
+    parser.add_argument('--seed', type=int, default=init_data.seed, metavar='S',
                         help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+    parser.add_argument('--log-interval', type=int, default=init_data.log_interval, metavar='N',
                         help='how many batches to wait before logging training status')
     args = parser.parse_args()
     kwargs = {'num_workers': 1, 'pin_memory': True}
+    return args, kwargs
 
-def load_mnist_data():
-    global args
-    global kargs
-    init_arguments()
+def load_mnist_data(init_data):
+    args, kwargs = init_arguments(init_data)
 
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(root='../data', train=True, download=True,
--- codes/sejin/nn_mnist/main.py
+++ codes/sejin/nn_mnist/main.py
@@ -1,2 +1,28 @@
+import torch
+from data_loader import load_mnist_data
+from neural_network import NeuralNetwork
+import tests
+
+class InitDataSet:
+    def __init__(self, batch_size=64, test_batch_size=1000, epochs=10, lr=0.01, momentum=0.5, no_cuda=True, seed=1,
+                 log_interval=10, ):
+        self.batch_size = batch_size
+        self.test_batch_size = test_batch_size
+        self.epochs = epochs
+        self.lr = lr
+        self.momentum = momentum
+        self.no_cuda = no_cuda
+        self.seed = seed
+        self.log_interval = log_interval
+
 if __name__ == '__main__':
-    pass
+    init_data = InitDataSet()
+
+    train_loader, test_loader = load_mnist_data(init_data)
+    tests.load_test(train_loader)
+    tests.load_test(test_loader)
+
+    nn = NeuralNetwork(init_data)
+    nn.train(train_loader)
+    # tests.nn_test(nn)
+    # nn.train()
