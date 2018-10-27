ddc4eef902135ecdcd2c59dd3d06c3c62f870682
Sejin Jeon (전세진) <jinaidy93@gmail.com>
2018-10-28 00:17:12 +0900

    Let tbrun.sh use multiple logdirs for Tensorboard (#26)

--- src/tbrun.sh
+++ src/tbrun.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 PS_RESULT=`ps -e | grep "tensorboard"`
+
+DRNN_RELATIVE="D-RNN/src/logs"
+GLOBAL=~/global/$DRNN_RELATIVE_LOG
+SEJIN=~/sejin/$DRNN_RELATIVE_LOG
+HYUNGSUN=~/hyungsun/$DRNN_RELATIVE_LOG
+YONGHA=~/yongha/$DRNN_RELATIVE_LOG
+KYUNGSOO=~/kyungsoo/$DRNN_RELATIVE_LOG
+
+# Execute Tensorflow if it doesn't run
 if [ -n $PS_RESULT ]
     then
-        tensorboard --logdir="logs" --port=9898 &
+        tensorboard --logdir="global:$GLOBAL,sejin:$SEJIN,hyungsun:$HYUNGSUN,yongha:$YONGHA,kyungsoo:$KYUNGSOO" --port=9898 &
         disown
     else
         echo tensorboard is already running in the background :\)
 fi
 
-#echo "=========================================================================="
-#echo "Enter the commands below to keep running if the ssh connection is closed."
-#echo "$ Ctrl + Z"
-#echo "$ bg"
-#echo "$ disown"
-#echo "=========================================================================="
-python train.py &
-disown
+# Start training if there is no option
+if [ -z "$1" ]
+    then
+        python train.py &
+        disown
+    elif [ $1 = "no" ]
+        then
+            echo Skip training.
+        else
+            echo Wrong parameter! Use \"no\" to skip training.
+fi
