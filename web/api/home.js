1b70f25a415d31138aba7b9facc60ed8a9168b87
Sejin Jeon <jinaidy93@gmail.com>
2018-11-03 18:53:29 +0900

    Enable mysql connection

--- web/api/home.js
+++ web/api/home.js
@@ -1,10 +1,35 @@
 import Router from 'koa-router';
+import pool from '../database.js';
 
 const home = new Router();
 
-const movies = ['test',/* mongoose */];
+const convertQuery = (queryResult, attrList) => {
+  const numAttrs = attrList.length;
+  const convertedQuery = {};
+  for(let j=0; j<numAttrs; j++) {
+    convertedQuery[attrList[j]] = queryResult[attrList[j]];
+  }
+  return convertedQuery;
+};
 
 home.get('/', async (ctx) => {
+  // pool.query('select * from movie', (error, results, fields) => {
+  //   if (error) console.log(error);
+  //   else {
+  //     movies.concat(results);
+  //   }
+  // });
+  const movies = [];
+  await pool.query('select * from movie', (error, results, fields) => {
+    const attrList = ['title', 'genre', 'play_time', 'post_url'];
+    if (error) console.log(error);
+    else {
+      const numResults = results.length;
+      for(let i=0; i<numResults; i++) {
+        movies.push(convertQuery(results[i], attrList));
+      }
+    }
+  });
   await ctx.render('home', {
     movies
   });
